3a868a5944e3b6de73093d4f858e9d50c4506535
adriannicolai <anlumbad@gmail.com>
2022-11-26 02:58:12 +0800

    add ajax for form submission

--- app/assets/javascripts/seats.js
+++ app/assets/javascripts/seats.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(document)
-    .on("submit", "#add_seat_form", submitAddSeatForm)
+    .on("submit", "#add_seat_form", submitAddSeatForm);
 })
 
 function submitAddSeatForm(e){
@@ -8,6 +8,14 @@ function submitAddSeatForm(e){
   let add_seat_form = $(this);
 
   $.post(add_seat_form.attr("Action"), add_seat_form.serialize(), function(add_seat_form_response){
-    console.log('add_seat_form_response :>> ', add_seat_form_response);
+    if (add_seat_form_response.status){
+      $("#airplane_seats").text(JSON.stringify(add_seat_form_response.result.airplane_seats));
+      $("#unboarded_passengers").text(add_seat_form_response.result.unboarded_passengers)
+    }
+    else{
+      if (add_seat_form_response.error){
+        alert(add_seat_form_response.error);
+      }
+    }
   })
 }
